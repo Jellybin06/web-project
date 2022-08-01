b7b91a64b14a2e7ce57029565d929c61a7cb1b9c
Jellybin06 <pjbboseung0817@gmail.com>
2022-08-01 20:08:20 +0900

    vc_page_0801

--- gun_menu_AR.js
+++ gun_menu_AR.js
@@ -1,3 +1,6 @@
 function m4() {
-    
+    var elems = document.getElementsByClassName('container');
+    for(var i = 0; i < elems.length; i+=1) {
+        elems[i].style.display = 'block';
+    }
 }
